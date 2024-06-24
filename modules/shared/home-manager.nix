bed09a93a2b8d88a5b12645a807e9dbd17e5944b
Nikola Milojević <nikola@milojevic.me>
2024-06-24 12:46:58 +0200

    sorting

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -1,17 +1,16 @@
 { config, pkgs, lib, ... }:
 
-
 {
   imports = [
     ./programs/atuin.nix
     ./programs/direnv.nix
+    ./programs/doggo.nix
     ./programs/fish.nix
     ./programs/git.nix
     ./programs/k9s.nix
+    ./programs/krew.nix
+    ./programs/neovim.nix
     ./programs/starship.nix
     ./programs/zoxide.nix
-    ./programs/neovim.nix
-    ./programs/krew.nix
-    ./programs/doggo.nix
   ];
 }
