67272944c214e6f68a4238d3051ce9666bc27206
Nikola Milojević <nikola@milojevic.me>
2024-08-16 10:18:59 +0200

    remove copilot and switch to supermaven

--- modules/shared/programs/neovim/nvim/lua/community.lua
+++ modules/shared/programs/neovim/nvim/lua/community.lua
@@ -2,30 +2,13 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.dracula-nvim" },
-  { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.project.project-nvim" },
-  { import = "astrocommunity.project.nvim-spectre" },
+  { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.editing-support.chatgpt-nvim" },
   { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
-  {
-    -- further customize the options set by the community
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
 }
--- modules/shared/programs/neovim/nvim/lua/plugins/chezmoi.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "alker0/chezmoi.vim",
-  init = function() vim.g["chezmoi#use_tmp_buffer"] = true end,
-}
