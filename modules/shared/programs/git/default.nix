e1835fd045cb691aeb4d14842af5d875a14d72fd
Nikola Milojević <nikola@milojevic.me>
2026-05-04 22:31:44 +0200

    ignore claude local settings in all folders

--- modules/shared/programs/git/default.nix
+++ modules/shared/programs/git/default.nix
@@ -1,15 +1,18 @@
-{ config, ... }:
+{ ... }:
 {
   programs.git = {
     enable = true;
     signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrYywYsK/kocVOa48LjaOR2X10g7lwsB1PtkyBJX800";
+
+    ignores = [
+      ".claude/settings.local.json"
+    ];
 
     settings = {
       user.name = "Nikola Milojević";
       user.email = "nikola@milojevic.me";
       pull.rebase = false;
       init.defaultBranch = "main";
-      core.excludesFile = "${config.home.homeDirectory}/.gitignore";
       commit.gpgSign = true;
       tag.gpgSign = true;
       gpg.format = "ssh";
